be04a8acfeb99fb0a05829593d128b6492ead8a9
Anil9899 <ay19869@gmail.com>
2025-04-26 11:11:06 +0530

    initial commit

---
+++ admin/admin/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import Admin from './pages/Admin/admin';
+
+
+function App() {
+  return (
+  
+    <div>
+      <Navbar />
+      <Admin />
+    </div>
+  )
+}
+
+export default App;
---
+++ backend/utils/databaseutils.js
@@ -0,0 +1,15 @@
+/*const mongo = require('mongodb');
+
+const MongoClient = mongo.MongoClient;
+
+const MONGO_URL = "mongodb+srv://ay19869:ZXNMOPQW@123@cluster0.ke9e8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const mongoconnect = (callback) => {
+    MongoClient.connect(MONGO_URL).then(client => {
+        callback(client);
+    }).catch(err => {
+        console.log('Error while connecting to Mongo:',err);
+    });
+}
+
+module.exports = mongoconnect;*/
